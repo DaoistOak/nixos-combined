ed51b6ea0b35e11ff67d7f120dc779f73f2cadf8
DaoistOak <shridal99@gmail.com>
2025-12-17 21:49:28 +0545

    Comment out Hyprspace plugin due to build failure

--- config/hypr/settings/plugins.nix
+++ config/hypr/settings/plugins.nix
@@ -3,7 +3,8 @@
 
 {
   wayland.windowManager.hyprland = {
-    plugins = [ pkgs.hyprlandPlugins.hyprgrass inputs.Hyprspace.packages.${pkgs.system}.Hyprspace ];
+    plugins = [ pkgs.hyprlandPlugins.hyprgrass # inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
+    ];
 
           settings = {
             plugin = {
