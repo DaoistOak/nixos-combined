e1b54b229017a94632ffacc2937dd35601cec16e
DaoistOak <shridal99@gmail.com>
2025-12-17 21:41:22 +0545

    Add Hyprspace plugin to plugins.nix and bind Super+Q to overview:toggle

--- config/hypr/settings/plugins.nix
+++ config/hypr/settings/plugins.nix
@@ -1,9 +1,9 @@
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   wayland.windowManager.hyprland = {
-    plugins = [ pkgs.hyprlandPlugins.hyprgrass ];
+    plugins = [ pkgs.hyprlandPlugins.hyprgrass inputs.Hyprspace.packages.${pkgs.system}.Hyprspace ];
 
           settings = {
             plugin = {
