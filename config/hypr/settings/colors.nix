3a0cd795107a9de7dc72b58d387e2af1828eeb45
DaoistOak <shridal99@gmail.com>
2025-12-20 12:59:12 +0545

    Update: 2025-12-20 12:59:12 (4 files changed)

--- config/hypr/settings/colors.nix
+++ config/hypr/settings/colors.nix
@@ -1,10 +1,12 @@
 {
   config,
+  lib,
   inputs,
   pkgs,
   ...
 }:
 {
   wayland.windowManager.hyprland.settings = {
+    general."col.active_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base0E})";
   };
 }
