8c21df2364cba4a062788b7592af5eb3fe80e211
DaoistOak <shridal99@gmail.com>
2026-03-10 00:28:53 +0545

    Update: 2026-03-10 00:28:53 (3 files changed)

--- config/hypr/settings/plugins.nix
+++ config/hypr/settings/plugins.nix
@@ -8,67 +8,10 @@
 {
   wayland.windowManager.hyprland = {
     plugins = [
-      inputs.hyprgrass.packages.${pkgs.system}.hyprgrass
-      # inputs.hyprland-plugins.packages.${pkgs.system}.hyprgrass
-      # pkgs.hyprlandPlugins.hyprscrolling
     ];
 
     settings = {
       plugin = {
-        hyprgrass = { };
-        hyprscrolling = {
-          column_width = 0.5;
-          fullscreen_on_one_column = true;
-        };
-
-        touch_gestures = {
-          # The default sensitivity is probably too low on tablet screens,
-          # I recommend turning it up to 4.0
-          sensitivity = 4.0;
-
-          # must be >= 3
-          workspace_swipe_fingers = 3;
-
-          # switching workspaces by swiping from an edge, this is separate from workspace_swipe_fingers
-          # and can be used at the same time
-          # possible values: l, r, u, or d
-          # to disable it set it to anything else
-          workspace_swipe_edge = "d";
-
-          # in milliseconds
-          long_press_delay = 400;
-
-          # resize windows by long-pressing on window borders and gaps.
-          # If general:resize_on_border is enabled, general:extend_border_grab_area is
-          # used for floating windows
-          resize_on_border_long_press = true;
-
-          # in pixels, the distance from the edge that is considered an edge
-          edge_margin = 10;
-
-          # emulates touchpad swipes when swiping in a direction that does not trigger
-          # workspace swipe. ONLY triggers when finger count is equal to
-          # workspace_swipe_fingers.
-          #
-          # might be removed in the future in favor of event hooks
-          emulate_touchpad_swipe = false;
-
-          experimental = {
-            send_cancel = 0;
-          };
-
-          # hyprgrass bindings
-          "hyprgrass-bind" = [
-            ", swipe:3:r, workspace, +1"
-            ", swipe:3:l, workspace, -1"
-            ", tap:3, exec, kitty"
-          ];
-
-          "hyprgrass-bindm" = [
-            ", longpress:2, movewindow"
-          ];
-
-        };
       };
     };
   };
