5c7a03d6373a71a34880bf3167cab551b60dad76
DaoistOak <shridal99@gmail.com>
2026-06-15 16:35:13 +0545

    fix deprecation: wineWowPackages -> wineWow64Packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -194,7 +194,7 @@ let
     gst_all_1.gst-plugins-good
     gst_all_1.gst-plugins-bad
     gst_all_1.gst-plugins-ugly
-    wineWowPackages.stable
+    wineWow64Packages
 
     # Utilities
     adwaita-icon-theme
