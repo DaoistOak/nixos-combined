ba7b5c3246d24f7e02afb750dc99fb33e8dbe723
DaoistOak <shridal99@gmail.com>
2026-06-15 16:34:14 +0545

    fix libvirt socket activation; replace wine with wineWowPackages.stable

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -194,7 +194,7 @@ let
     gst_all_1.gst-plugins-good
     gst_all_1.gst-plugins-bad
     gst_all_1.gst-plugins-ugly
-    wine
+    wineWowPackages.stable
 
     # Utilities
     adwaita-icon-theme
