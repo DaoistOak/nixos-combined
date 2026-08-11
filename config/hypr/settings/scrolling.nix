eeaee2e068e8eba798ad9d6779181f8c6a45aab8
DaoistOak <shridal99@gmail.com>
2026-08-11 15:25:59 +0545

    Update: 2026-08-11 15:25:59 (1 files changed)

--- config/hypr/settings/scrolling.nix
+++ config/hypr/settings/scrolling.nix
@@ -10,11 +10,11 @@
 
         scrolling = {
           fullscreen_on_one_column = true;
-          column_width = 0.5;
+          column_width = 0.4;
           focus_fit_method = 1;
           follow_focus = true;
           follow_min_visible = 0.4;
-          explicit_column_widths = "0.5, 0.8, 1.0";
+          explicit_column_widths = "0.4, 0.5, 0.8, 1.0";
           wrap_focus = true;
           wrap_swapcol = true;
           direction = "right";
