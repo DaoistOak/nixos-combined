2651173b8b8e7e664c4e2c07a36c2dab7f808591
DaoistOak <shridal99@gmail.com>
2026-08-14 14:37:05 +0545

    Update: 2026-08-14 14:37:05 (1 files changed)

--- config/hypr/settings/scrolling.nix
+++ config/hypr/settings/scrolling.nix
@@ -9,17 +9,17 @@
         };
 
         scrolling = {
-          fullscreen_on_one_column = true;
+          fullscreen_on_one_column = "true";
           column_width = 0.47;
           focus_fit_method = 1;
-          follow_focus = true;
+          follow_focus = "true";
           follow_min_visible = 0;
           explicit_column_widths = "0.4, 0.5, 0.8, 1.0";
-          wrap_focus = true;
-          wrap_swapcol = true;
+          wrap_focus = "true";
+          wrap_swapcol = "true";
           direction = "right";
-          layout_aware = true;
         };
+          layout_aware = "true";
       };
     };
   };
