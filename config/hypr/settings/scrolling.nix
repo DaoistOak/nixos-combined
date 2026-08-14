4b09c369f2ba10cc885e965a666fcc007f49d3b1
DaoistOak <shridal99@gmail.com>
2026-08-14 14:31:38 +0545

    Update: 2026-08-14 14:31:38 (1 files changed)

--- config/hypr/settings/scrolling.nix
+++ config/hypr/settings/scrolling.nix
@@ -13,7 +13,7 @@
           column_width = 0.48;
           focus_fit_method = 1;
           follow_focus = true;
-          follow_min_visible = 0.4;
+          follow_min_visible = 0;
           explicit_column_widths = "0.4, 0.5, 0.8, 1.0";
           wrap_focus = true;
           wrap_swapcol = true;
