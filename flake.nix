79e596921b74c0197e0206d5a9108debef173083
DaoistOak <shridal99@gmail.com>
2026-09-09 22:44:57 +0545

    Update: 2026-09-09 22:44:57 (2 files changed)

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland/a0136d8c04687bb36eb8a28eb9d1ff92aea99704"; # v0.55.4 (downgraded from 0.56 while noctalia needs activeWorkspace.id in j/monitors)
+    hyprland.url = "github:hyprwm/Hyprland/5c9377c15f85c50648f35ca5a213754f95b93ca0"; # v0.56.1 (has input.keyboard.key event AND activeWorkspace.id for noctalia)
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
