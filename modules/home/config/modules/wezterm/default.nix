58773f61f1da2e483bcb44bcf38aba711d348815
DaoistOak <shridal99@gmail.com>
2026-09-13 23:03:06 +0545

    Update: 2026-09-13 23:03:06 (1 files changed)

--- modules/home/config/modules/wezterm/default.nix
+++ modules/home/config/modules/wezterm/default.nix
@@ -22,7 +22,7 @@
       default_prog = [ "zsh" ];
       # Don't ask for confirmation when closing a window (WM/decoration close).
       window_close_confirmation = "NeverPrompt";
-      window_background_opacity = 0.85;
+      window_background_opacity = 1.0;
 
       # --- Animations: synced to the Hyprland animation profile ---
       # Display refresh is 1920x1200@60 (modules/config/hypr/settings/displays.nix);
